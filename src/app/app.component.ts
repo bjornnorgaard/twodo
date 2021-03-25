3d739bd733c94d4d2fafe3ad77c587aee72d1f8e
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2021-03-25 19:16:15 +0100

    Added github link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-
+  public goToSource(): void {
+    window.location.href = 'https://github.com/bjornnorgaard/twoodo';
+  }
 }
