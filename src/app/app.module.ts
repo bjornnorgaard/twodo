1c304479d2d9e92c3355c8b1647da6f49519b2b8
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2021-03-25 17:03:00 +0100

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputFieldComponent } from './input-field/input-field.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFieldComponent,
+    TodoListComponent,
+    TodoListItemComponent
   ],
   imports: [
     BrowserModule,
